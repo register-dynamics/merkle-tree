4caeccd5d2857ce9114196b42b489cb97722cf3f
Andy Bennett <andyjpb@digital.cabinet-office.gov.uk>
2016-04-19 14:48:13 +0100

    Rework the tests such that the root hash for the empty tree is not a special case
    This paves the way for a kinder implementation of the merkle-tree-append
    tests as they will build up each leaf of the reference tree, starting
    with the empty tree.
    
    Signed-off-by: Andy Bennett <andyjpb@digital.cabinet-office.gov.uk>

--- tests/run.scm
+++ tests/run.scm
@@ -93,7 +93,8 @@
 
 ; https://github.com/google/certificate-transparency/blob/master/cpp/merkletree/merkle_tree_test.cc#L269
 (let ((roots ; Incremental roots from building the reference tree from inputs leaf-by-leaf.
-	'(#${6e340b9cffb37a989ca544e6bb780a2c78901d3fb33738768511a30617afa01d}
+	'(#${e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855} ; empty
+	  #${6e340b9cffb37a989ca544e6bb780a2c78901d3fb33738768511a30617afa01d} ; 1 leaf
 	  #${fac54203e7cc696cf0dfcb42c92a1d9dbaf70ad9e621f4bd8d98662f00e3c125}
 	  #${aeb6bcfe274b70a14fb067a5e5578264db0fa9b51af5e0ba159158f329e06e77}
 	  #${d37ee418976dd95753c1c73862b9398fa2a2cf9b4ff0fdfe8b30cd95209614b7}
@@ -102,7 +103,7 @@
 	  #${ddb89be403809e325750d3d263cd78929c2942b7942a34b77e122c9594a74c8c}
 	  #${5dc9da79a70659a9ad559cb701ded9a2ab9d823aad2f4960cfe370eff4604328})))
 
-  (assert (= (length reference-leaves) (length roots)))
+  (assert (= (length reference-leaves) (- (length roots) 1)))
 
   (test-group
     "Incremental roots from building the reference tree from inputs leaf-by-leaf."
@@ -113,14 +114,14 @@
 	    (conc "Tree with " n " leaves")
 	    (test
 	      "Root hash"
-	      (list-ref roots (- n 1))
+	      (list-ref roots n)
 	      (dense-merkle-tree-hash tree))
 	    (let loop ((n 1))
 	      (if (<= n (merkle-tree-size tree))
 		(begin
 		  (test ; Test each potential sub-tree has the correct root hash.
 		    (conc "First " n " leaves")
-		    (list-ref roots (- n 1))
+		    (list-ref roots n)
 		    (dense-merkle-tree-hash tree 0 n))
 		  (loop (+ 1 n))))))
 	  (loop (+ 1 n))))))))
